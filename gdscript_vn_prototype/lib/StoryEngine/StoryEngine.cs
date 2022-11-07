3d56a0d88c351e5f90c28616a4dadb0a8ccd2669
Sabrina Gannon <sabrinagannon@icloud.com>
2022-11-06 22:15:24 -0400

    Attempt to rename storyengineapi class

--- gdscript_vn_prototype/lib/StoryEngine/StoryEngine.cs
+++ gdscript_vn_prototype/lib/StoryEngine/StoryEngine.cs
@@ -2,11 +2,11 @@ using StoryEngine.StoryFundamentals;
 
 namespace StoryEngine
 {
-	public class StoryEngineAPI
+	public class StoryEngine
 	{
 		//private Story? _story;
 
-		public StoryEngineAPI(string storyFilename, string elementCollectionFilename)
+		public StoryEngine(string storyFilename, string elementCollectionFilename)
 		{
 			// Load story and element collection
 			// Create new story
