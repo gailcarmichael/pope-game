933bc7d4e5be3cfe5b11d619b17478509c0a3711
Alec McGrail <alecmcgrail@gmail.com>
2023-04-01 18:32:53 -0400

    Print from StoryExplorer to Screen
    Added to Sabrina's code so that the text received from the storyEngine is printed on the screen.
    
    - Added a /Scripts to /Assets

--- Assets/Scripts/GodotTest.gd
+++ Assets/Scripts/GodotTest.gd
@@ -1,17 +1,20 @@
 extends Node
 
-
-# var storynode = storyengine.new()
+var my_csharp_script = load("res://Assets/Scripts/MyCSharpNode.cs") # needs to have the same name as the class!
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# This code basically lifted from https://docs.godotengine.org/en/stable/tutorials/scripting/cross_language_scripting.html
-	var my_csharp_script = load("res://MyCSharpNode.cs") # needs to have the same name as the class !
+
 	var my_csharp_node = my_csharp_script.new()
 	var text_from_csharp = my_csharp_node.TeaserText()
-	print(text_from_csharp) # outputs "node 2 teaser" 
+	print("Ready(): " + text_from_csharp) # outputs "node 2 teaser" 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	var my_csharp_node = my_csharp_script.new()
+	var text_from_csharp = my_csharp_node.TeaserText()
+	get_node("DialogBox/DialogText").text = "Process(): " + text_from_csharp
+	
 	pass
--- Assets/Scripts/MyCSharpNode.cs
+++ Assets/Scripts/MyCSharpNode.cs
@@ -4,8 +4,9 @@ using StoryEngine;
 
 public partial class MyCSharpNode : Node
 {
+	private StoryEngineAPI	storyEngine = new StoryEngineAPI("filename", "otherFileName");
+	
 	public string TeaserText() {
-		StoryEngineAPI storyEngine = new StoryEngineAPI("filename", "otherFilename");
 		return storyEngine.CurrentNodeTeaserText();
 	}
 }
